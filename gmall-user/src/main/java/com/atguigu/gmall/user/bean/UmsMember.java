b140086f986f35d9836d19d0675e58c49b369d80
jiwei <13165782821@163.com>
2019-08-14 16:23:16 +0800

    第二次提交

---
+++ gmall-user/src/main/java/com/atguigu/gmall/user/bean/UmsMember.java
@@ -0,0 +1,40 @@
+package com.atguigu.gmall.user.bean;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.util.Date;
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor(force = true)
+public class UmsMember {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private String id;
+    private String memberLevelId;
+    private String username;
+    private String password;
+    private String nickname;
+    private String phone;
+    private int status;
+    private Date createTime;
+    private String icon;
+    private int gender;
+    private Date birthday;
+    private String city;
+    private String job;
+    private String personalizedSignature;
+    private int sourceType;
+    private int integration;
+    private int growth;
+    private int luckeyCount;
+    private int historyIntegration;
+
+
+}
